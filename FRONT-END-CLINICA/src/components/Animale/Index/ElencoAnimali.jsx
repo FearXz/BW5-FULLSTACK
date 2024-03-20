98a3cde40b9976f710983b6477732c9636eeec2e
Ebby Chakkungal <121340114+EbbyChack@users.noreply.github.com>
2024-03-20 14:48:38 +0100

    Add search functionality and styling to animal list

--- FRONT-END-CLINICA/src/components/Animale/Index/ElencoAnimali.jsx
+++ FRONT-END-CLINICA/src/components/Animale/Index/ElencoAnimali.jsx
@@ -1,31 +1,59 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchListaAnimali } from "../../../redux/actions/animale";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { Card, Col, Container, Form, Row } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
 export const ElencoAnimali = () => {
-const dispatch = useDispatch();
-    const animali = useSelector(state => state.animale.listaAnimali);
+  const dispatch = useDispatch();
+  const animali = useSelector((state) => state.animale.listaAnimali);
+  const [search, setSearch] = useState("");
 
+  useEffect(() => {
+    dispatch(fetchListaAnimali());
+  }, []);
 
-
-    useEffect(() => {
-		dispatch(fetchListaAnimali());
-	}, []);
-
+  const filteredAnimali = animali
+    ? animali.filter((animale) => animale.animale.nome.toLowerCase().includes(search.toLowerCase()))
+    : [];
 
   return (
-    <div>
-      <h1>Elenco Animali</h1>
-      <ul className="text-center">
-        {animali && animali.map((item) => (
-          <li className="my-3 border"  key={item.animale.idAnimale}>
-            {item.animale.nome} 
-            <Link className="btn btn-warning mx-4" to={`/animale/edit/${item.animale.idAnimale}`}>Modifica</Link>
-          </li>
-        ))}
+    <Container className="mt-3">
+      <h2 className="text-center">Lista animali</h2>
+      <div>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control type="email" placeholder="Cerca animale" onChange={(e) => setSearch(e.target.value)} />
+          </Form.Group>
+        </Form>
+      </div>
+      <Row className="g-3">
+        {filteredAnimali &&
+          filteredAnimali.map((obj, index) => (
+            <Col className="col-4" key={index}>
+              <Card className="bg-light bg-opacity-75">
+                <Card.Body>
+                  <Card.Title>{obj.animale.nome}</Card.Title>
+                  <div className="d-flex align-items-baseline">
+                    <Card.Subtitle>Specie</Card.Subtitle>
+                    <Card.Text className="ms-2">{obj.animale.specie}</Card.Text>
+                  </div>
 
-      </ul>
-    </div>
+                  <div className="d-flex justify-content-end">
+                    <Link className="btn btn-dark me-1" to={"/Aniamle/Details/" + obj.animale.idAnimale}>
+                      <FontAwesomeIcon icon={faInfoCircle} />
+                    </Link>
+                    <Link className="btn btn-dark" to={"/Animale/Edit/" + obj.animale.idAnimale}>
+                      <FontAwesomeIcon icon={faEdit} />
+                    </Link>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+      </Row>
+    </Container>
   );
-}
+};
