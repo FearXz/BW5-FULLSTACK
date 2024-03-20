071811e58f92d8495854f567cf8b516a86e9cfe9
egiros <148718730+egiros@users.noreply.github.com>
2024-03-20 15:22:11 +0100

    update navbar

--- FRONT-END-CLINICA/src/components/MyNavbar.jsx
+++ FRONT-END-CLINICA/src/components/MyNavbar.jsx
@@ -7,7 +7,7 @@ import { NavLink } from "react-router-dom";
 
 function MyNavbar() {
   return (
-    <div expand="lg" className=" navbar navbar-expand-lg  CustomNavBar">
+    <Navbar expand="lg" className=" navbar navbar-expand-lg  CustomNavBar">
       <Container>
         <NavbarBrand to={"/"}>
           <div className="d-flex flex-column align-items-center justify-content-center">
@@ -24,47 +24,46 @@ function MyNavbar() {
         </NavbarBrand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <div className="nav me-auto">
-            <NavLink className={"nav-link"} to="/">
-              Home
-            </NavLink>
+          <NavLink className={"nav-link"} to="/">
+            Home
+          </NavLink>
 
-            <NavDropdown title="Proprietari" id="basic-nav-dropdown">
-              <NavDropdown.Item to={"/Proprietario"} as={NavLink} className={"custom-dropdown-item"}>
-                Lista proprietari
-              </NavDropdown.Item>
-              <NavDropdown.Item to={"/Proprietario/Create"} as={NavLink} className={""}>
-                Aggiungi proprietario
-              </NavDropdown.Item>
-            </NavDropdown>
+          <NavDropdown title="Proprietari" id="basic-nav-dropdown">
+            <NavDropdown.Item to={"/Proprietario"} as={NavLink} className={"custom-dropdown-item"}>
+              Lista proprietari
+            </NavDropdown.Item>
+            <NavDropdown.Item to={"/Proprietario/Create"} as={NavLink} className={""}>
+              Aggiungi proprietario
+            </NavDropdown.Item>
+          </NavDropdown>
 
-            <NavDropdown title="Animali" id="basic-nav-dropdown">
-              <NavDropdown.Item to={"/Animale"} as={NavLink}>
-                Lista Animali
-              </NavDropdown.Item>
-              <NavDropdown.Item to={"/Animale/Create"} as={NavLink} className={""}>
-                Aggiungi animale
-              </NavDropdown.Item>
-            </NavDropdown>
+          <NavDropdown title="Animali" id="basic-nav-dropdown">
+            <NavDropdown.Item to={"/Animale"} as={NavLink}>
+              Lista Animali
+            </NavDropdown.Item>
+            <NavDropdown.Item to={"/Animale/Create"} as={NavLink} className={""}>
+              Aggiungi animale
+            </NavDropdown.Item>
+          </NavDropdown>
 
-            <NavDropdown title="Visite" id="basic-nav-dropdown">
-              <NavDropdown.Item to={"/Visite"} as={NavLink} className={""}>
-                Lista visite
-              </NavDropdown.Item>
-              <NavDropdown.Item to={"/Visite/Create"} as={NavLink} className={""}>
-                Aggiungi visita
-              </NavDropdown.Item>
-            </NavDropdown>
+          <NavDropdown title="Visite" id="basic-nav-dropdown">
+            <NavDropdown.Item to={"/Visite"} as={NavLink} className={""}>
+              Lista visite
+            </NavDropdown.Item>
+            <NavDropdown.Item to={"/Visite/Create"} as={NavLink} className={""}>
+              Aggiungi visita
+            </NavDropdown.Item>
+          </NavDropdown>
 
-            <NavDropdown title="Ricoveri" id="basic-nav-dropdown">
-              <NavDropdown.Item to={"/Ricovero"} as={NavLink} className={""}>
-                Lista ricoveri
-              </NavDropdown.Item>
-              <NavDropdown.Item to={"/Ricovero/Create"} as={NavLink} className={""}>
-                Aggiungi ricovero
-              </NavDropdown.Item>
-            </NavDropdown>
-          </div>
+          <NavDropdown title="Ricoveri" id="basic-nav-dropdown" className="me-auto">
+            <NavDropdown.Item to={"/Ricovero"} as={NavLink} className={""}>
+              Lista ricoveri
+            </NavDropdown.Item>
+            <NavDropdown.Item to={"/Ricovero/Create"} as={NavLink} className={""}>
+              Aggiungi ricovero
+            </NavDropdown.Item>
+          </NavDropdown>
+
           <div>
             <NavLink className={"nav-link"} to={"/login"}>
               Login
@@ -72,7 +71,7 @@ function MyNavbar() {
           </div>
         </Navbar.Collapse>
       </Container>
-    </div>
+    </Navbar>
   );
 }
 
