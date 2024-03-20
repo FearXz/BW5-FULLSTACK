e8f98a4ff1814ab5173de3d11871f06c207a58c4
Ebby Chakkungal <121340114+EbbyChack@users.noreply.github.com>
2024-03-20 10:33:11 +0100

    Add animal modal and functionality

--- FRONT-END-CLINICA/src/components/Proprietario/Details/DetailProprietario.jsx
+++ FRONT-END-CLINICA/src/components/Proprietario/Details/DetailProprietario.jsx
@@ -1,14 +1,16 @@
-import  { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchProprietarioById } from "../../../redux/actions/proprietario";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { Card, Col, Container, Row } from "react-bootstrap";
-
+import { Form, useParams } from "react-router-dom";
+import { Button, Card, Col, Container, Modal, Row } from "react-bootstrap";
 
 function DetailProprietario() {
   const dispatch = useDispatch();
   const { id } = useParams();
   const p = useSelector((state) => state.proprietario.singoloProprietario);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   useEffect(() => {
     dispatch(fetchProprietarioById(id));
@@ -33,13 +35,52 @@ function DetailProprietario() {
                   <strong>Numero telefono: </strong>
                   <span className="ms-2">{p.proprietario.numeroTelefono}</span>
                 </div>
-                <h5 className="mt-3">Animali</h5>
+                <div className="d-flex align-items-center my-3">
+                  <h5 className="m-0 me-1">Animali</h5>
+                  <Button variant="light" onClick={handleShow}>
+                    Aggiungi animale
+                  </Button>
+                </div>
+
+                <Modal show={show} onHide={handleClose}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Aggiungi animale</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    <Form>
+                      <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+                      </Form.Group>
+
+                      <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                      </Form.Group>
+                      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                      </Form.Group>
+                      <Button variant="primary" type="submit">
+                        Submit
+                      </Button>
+                    </Form>
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                      Chiudi
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                      Aggiungi
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
 
                 <Row>
                   {p.animali.map((obj, index) => (
-                    <Container>
+                    <Container key={index}>
                       <Col className="col-2">
-                        <Card className="bg-light bg-opacity-75" key={index}>
+                        <Card className="bg-light bg-opacity-75">
                           <Card.Body>
                             <div className="d-flex align-items-baseline">
                               <Card.Subtitle>Nome: </Card.Subtitle>
